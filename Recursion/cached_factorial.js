f61f8d77b16733e4684bc60637c59f8770c1efc7
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-08-29 23:21:08 +0100

    Add recursive factorial solution with caching

---
+++ Recursion/cached_factorial.js
@@ -0,0 +1,23 @@
+const factorial = () => {
+  const cache = {}
+
+  return (n) => {
+    if (n == 1) {
+      return 1
+    } else {
+      if (n in cache) {
+        return cache[n]
+      } else {
+        const cachedFactorial = cache[n-1] || factorial()(n-1) 
+        result = n * cachedFactorial
+        cache[n] = result
+        return result
+      }
+    }
+  }
+}
+
+const cachedFactorial = factorial()
+console.log(cachedFactorial(5)) // Computes the factorial of 5 the first time
+console.log(cachedFactorial(5)) // Gets the cached factorial of 5
+console.log(cachedFactorial(6)) // Computes 6 * the cached factorial of 5
